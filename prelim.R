d14132ddf393d318637956bb89e8d0537de0cfca
Carlvin Mwange <cmwange@innova.co.ke>
2020-06-11 01:36:40 +0300

    Initial set up for resume

---
+++ prelim.R
@@ -0,0 +1,11 @@
+# devtools::install_github("nstrayer/datadrivencv")
+#
+# # run ?datadrivencv::use_datadriven_cv to see more details
+# datadrivencv::use_datadriven_cv(
+#   full_name = "MWANGE CARLVIN JERRY",
+#   data_location = "https://docs.google.com/spreadsheets/d/1KZ3za7zbPXUITjnWR_f9tvrrtTnce_W1YoSxhqrFkJw/edit?usp=sharing",
+#   pdf_location = "https://github.com/CarlvinJerry/Resume/carlvin_cv.pdf",
+#   html_location = "carlvinjerry.me/cv/",
+#   source_location = "https://github.com/CarlvinJerry/Resume"
+# )
+#
